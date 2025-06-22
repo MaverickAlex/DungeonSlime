bb54b8c4cee634ee4d7b61e1102c2e968c708e31
MaverickAlex <maverick.alex@gmail.com>
2025-06-22 10:19:24 -0600

    Chapter 15

--- Game1.cs
+++ Game1.cs
@@ -43,6 +43,9 @@ public class Game1 : Core
   // The sound effect to play when the slime eats a bat.
   private SoundEffect _collectSoundEffect;
 
+  // The background theme song
+  private Song _themeSong;
+
   public Game1() : base("Dungeon Slime", 1280, 720, false)
   {
   }
@@ -69,6 +72,8 @@ public class Game1 : Core
 
     // Assign the initial random velocity to the bat.
     AssignRandomBatVelocity();
+    // Start playing the background music
+    Audio.PlaySong(_themeSong);
   }
 
   protected override void LoadContent()
@@ -95,19 +100,7 @@ public class Game1 : Core
     _collectSoundEffect = Content.Load<SoundEffect>("audio/collect");
 
     // Load the background theme music
-    Song theme = Content.Load<Song>("audio/theme");
-
-    // Ensure media player is not already playing on device, if so, stop it
-    if (MediaPlayer.State == MediaState.Playing)
-    {
-      MediaPlayer.Stop();
-    }
-
-    // Play the background theme music.
-    MediaPlayer.Play(theme);
-
-    // Set the theme music to repeat.
-    MediaPlayer.IsRepeating = true;
+    _themeSong = Content.Load<Song>("audio/theme");
   }
 
   protected override void Update(GameTime gameTime)
@@ -207,7 +200,7 @@ public class Game1 : Core
       _batVelocity = Vector2.Reflect(_batVelocity, normal);
 
       // Play the bounce sound effect
-      _bounceSoundEffect.Play();
+      Audio.PlaySoundEffect(_bounceSoundEffect);
     }
 
     _batPosition = newBatPosition;
@@ -227,7 +220,7 @@ public class Game1 : Core
       AssignRandomBatVelocity();
 
       // Play the collect sound effect
-      _collectSoundEffect.Play();
+      Audio.PlaySoundEffect(_collectSoundEffect);
     }
 
     base.Update(gameTime);
@@ -278,6 +271,26 @@ public class Game1 : Core
     if (Input.Keyboard.IsKeyDown(Keys.D) || Input.Keyboard.IsKeyDown(Keys.Right))
     {
       _slimePosition.X += speed;
+    }
+
+    // If the M key is pressed, toggle mute state for audio.
+    if (Input.Keyboard.WasKeyJustPressed(Keys.M))
+    {
+      Audio.ToggleMute();
+    }
+
+    // If the + button is pressed, increase the volume.
+    if (Input.Keyboard.WasKeyJustPressed(Keys.OemPlus))
+    {
+      Audio.SongVolume += 0.1f;
+      Audio.SoundEffectVolume += 0.1f;
+    }
+
+    // If the - button was pressed, decrease the volume.
+    if (Input.Keyboard.WasKeyJustPressed(Keys.OemMinus))
+    {
+      Audio.SongVolume -= 0.1f;
+      Audio.SoundEffectVolume -= 0.1f;
     }
   }
 
